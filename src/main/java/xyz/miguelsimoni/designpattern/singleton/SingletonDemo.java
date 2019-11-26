947c01809b0dbc10c15dac3f81bad0dc9c1168ba
Miguel.Simoni <miguel.simoni@gmail.com>
2019-11-25 23:07:37 -0300

    Initial commit.

---
+++ src/main/java/xyz/miguelsimoni/designpattern/singleton/SingletonDemo.java
@@ -0,0 +1,19 @@
+package xyz.miguelsimoni.designpattern.singleton;
+
+/**
+ * Singleton Demo App
+ */
+public class SingletonDemo 
+{
+    public static void main(String[] args)
+    {
+        System.out.println( "--- Singleton Demo App ---" );
+
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.out.println("s1 -> " + s1.toString());
+        System.out.println("s2 -> " + s2.toString());
+        System.out.println("s1 == s2 -> " + (s1 == s2));
+    }
+}
---
+++ src/test/java/xyz/miguelsimoni/designpattern/singleton/SingletonTest.java
@@ -0,0 +1,23 @@
+package xyz.miguelsimoni.designpattern.singleton;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Singleton.
+ */
+public class SingletonTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        assertTrue( s1 == s2 );
+    }
+}
