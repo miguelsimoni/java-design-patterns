40190f98f68ff57c61050fa2c28b20f63052c60d
Miguel.Simoni <miguel.simoni@gmail.com>
2019-12-23 12:35:11 -0300

    Add design pattern: Singleton.

---
+++ src/main/java/xyz/miguelsimoni/designpattern/creational/singleton/EagerSingleton.java
@@ -0,0 +1,19 @@
+package xyz.miguelsimoni.designpattern.creational.singleton;
+
+/**
+ * Singleton Design Pattern.
+ */
+public class EagerSingleton {
+
+    /**
+     * Single instance.
+     */
+    private static final EagerSingleton instance = new EagerSingleton();
+
+    /**
+     * Return the current instance of the class. Create it if does not exist.
+     */
+    public static EagerSingleton getInstance() {
+        return instance;
+    }    
+}
---
+++ src/main/java/xyz/miguelsimoni/designpattern/creational/singleton/Singleton.java
@@ -0,0 +1,27 @@
+package xyz.miguelsimoni.designpattern.creational.singleton;
+
+/**
+ * Singleton Design Pattern.
+ */
+public class Singleton {
+
+    /**
+     * Single instance.
+     */
+    private static Singleton instance;
+
+    /**
+     * Private default constructor prevents the class to be instantiated by other means.
+     */
+    private Singleton() {}
+
+    /**
+     * Return the current instance of the class. Create it if does not exist.
+     */
+    public static Singleton getInstance() {
+        if(instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }    
+}
